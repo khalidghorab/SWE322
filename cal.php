d78eaa688a5941703bf21aaeec838be37c8317bd
khalidghorab <116163449+khalidghorab@users.noreply.github.com>
2023-09-11 15:15:30 +0300

    Add files via upload

---
+++ cal.php
@@ -0,0 +1,36 @@
+<html>
+    <?php  
+    
+function add($x, $y){
+return ($x+$y);
+}
+    
+function subtract($x, $y){
+return ($x-$y);
+}
+    
+function multiply($x, $y){
+return ($x*$y);
+}
+    
+function divide($x, $y){
+return ($x/$y);
+}
+    
+ echo "3 + 5 =  "  . add(3,5);
+    echo "<br>";
+ echo "10 / 2 = " . divide(10,2);
+    echo "<br>";
+ echo "10 * 5 = " . multiply(10,2);
+    echo "<br>";
+ echo "10 - 5 = " . subtract(10,5);
+    echo "<br>";
+    
+    
+    
+
+        
+        ?>
+
+
+</html> 
